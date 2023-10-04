1757a5d41dc86618d19b39f7274bf05508c0ca2e
Surafel <surafeltakele87@gmail.com>
2023-10-05 00:21:43 +0300

    missing number leetcode

---
+++ missingNumber/missingnumber.py
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(std::vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+};
+
